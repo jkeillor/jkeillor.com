d98bb7f6c26e5c0967878b9a81cc46f2087e82e7
Jeremy Keillor <jkeillor@gmail.com>
2015-09-13 22:12:55 -0500

    Render posts from database, fix bug in model.

--- website.py
+++ website.py
@@ -18,6 +18,7 @@ app = web.application(urls, locals())
 
 render = web.template.render(settings.get('Settings','templates'), base='sidebar')
 plain = web.template.render(settings.get('Settings', 'templates'))
+extensions = ['markdown.extensions.extra', 'markdown.extensions.smarty']
 
 class home:
     def GET(self):
@@ -28,16 +29,18 @@ class page:
         page = model.get_page(name)
         pages = model.get_pages()
         if bool(page):
-            content = self.__getContent(name)
+            content = self.__getContent(page, pg)
             pgs = plain.pages(dict(page), pages)
             return render.page(dict(page), content, pgs)
         else:
             raise web.seeother('/home')
 
-    def __getContent(self, name):
-        content = StringIO.StringIO()
-        markdown.markdownFromFile(input = settings.get("Settings", 'content') + name, output = content, extensions = ['markdown.extensions.extra', 'markdown.extensions.smarty'])
-        return dict(render = content.getvalue())
+    def __getContent(self, page, offset):
+        posts = model.get_page_contents(page.name, page.pagesize, offset)
+        if posts:
+            return [plain.post(x.title, markdown.markdown(x.content, extensions=extensions)) for x in list(posts)]
+        else:
+            return []
 
 if __name__ == "__main__":
     app.run()
